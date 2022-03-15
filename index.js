eddfdffa4904eafd3be77252af70ecbf0f146283
Tyler <tylerclancy030@gmail.com>
2022-03-14 23:02:41 -0500

    Add get route to return restaurants

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.get('/restaurants', (req, res) => {
+  res.send(restaurants);
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000!');
 });
