a2e2d2d4857cb138a61fa60ad2e7a9839a3d011d
Tyler <tylerclancy030@gmail.com>
2022-03-14 23:17:32 -0500

    Add post and delete routes

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ app.get('/restaurants', (req, res) => {
   res.send(restaurants);
 });
 
+app.post('/restaurant', (req, res) => {
+  restaurants.push({ id: req.body.id, name: req.body.name });
+  res.send(`${restaurants} created.`);
+});
+
+app.delete('/restaurant/:id', (req, res) => {
+  console.log('delete:id'+req.params.id);
+  restaurants = restaurants.filter(item => item.id != req.params.id);
+  res.send('restaurants left:'+JSON.stringify(restaurants));
+})
+
 app.listen(3000, () => {
   console.log('Server started on port 3000!');
 });
