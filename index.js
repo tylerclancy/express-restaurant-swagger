3ff6f19da6e32dc975b8187503e548a6335e34c1
Tyler <tylerclancy030@gmail.com>
2022-03-14 23:00:52 -0500

    Declare restaurants array

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+var restaurants = [
+  { id: 0, name: 'Woodshill' },
+  { id: 1, name: 'Fiorellas' },
+];
+
 app.listen(3000, () => {
   console.log('Server started on port 3000!');
-})
+});
